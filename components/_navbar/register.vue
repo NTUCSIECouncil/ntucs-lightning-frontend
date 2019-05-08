3c7eaa5b288a3d6efd88818b36c9d5b92d7aa424
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-09 00:19:07 +0800

    - add register component

--- components/_navbar/register.vue
+++ components/_navbar/register.vue
@@ -1,51 +1,89 @@
 <template>
   <div>
-    <b-form>
-      <b-form-group label="First Name">
-        <b-form-input
-          required
-          placeholder="First Name"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group label="Lase Name">
+    <b-form @submit="userRegister">
+      <b-form-group label="姓">
         <b-form-input
           required
           placeholder="Lase Name"
+          type="lastname"
+          v-model="userRegisterForm.name.last"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group label="Preferred Name">
+      <b-form-group label="名">
+        <b-form-input
+          required
+          placeholder="First Name"
+          type="firstname"
+          v-model="userRegisterForm.name.first"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="暱稱">
         <b-form-input
           required
           placeholder="Preferred Name"
+          type="nickname"
+          v-model="userRegisterForm.name.preferred"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group label="Email">
+      <b-form-group label="電子郵件">
         <b-form-input
           required
           placeholder="Email"
+          type="email"
+          v-model="userRegisterForm.email"
         ></b-form-input>
       </b-form-group>
 
-      <b-form-group label="Password">
+      <b-form-group label="密碼">
         <b-form-input
           required
           placeholder="Password"
+          type="password"
+          v-model="userRegisterForm.password"
         ></b-form-input>
       </b-form-group>
-    </b-form>
 
-    <label for="">
+      <p></p>
+      <d-button block-level theme="primary" type="submit">
+        註冊
+      </d-button>
+  
+    </b-form>
+    <br />
+    <div class="alreadyMember text-center" v-on:click="alreadyMember()">
       已經有 TheChanger 帳號？按此登入。
-    </label>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: '_navbarRegister'
+  name: 'navbarRegister',
+  data () {
+    return {
+      userRegisterForm: {
+        name: {
+          last: '',
+          first: '',
+          preferred: ''
+        },
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    alreadyMember () {
+      this.$store.commit('navbar/setAccountFormType', 'signin')
+      this.$store.commit('navbar/setAccountFormText', 'signin')
+    },
+    userRegister () {
+      this.$store.dispatch('navbar/userRegister', this.userRegisterForm)
+    }
+  }
 }
 </script>
 
