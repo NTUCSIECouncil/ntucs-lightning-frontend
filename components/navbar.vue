7308ee734338b376c911374b4e3c113693af576d
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-09 00:19:24 +0800

    - update navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,12 +11,11 @@
             <!-- Right aligned nav items -->
             <d-navbar-nav class="ml-auto">
               <d-nav-item href="#">成為作者</d-nav-item>
-              <d-nav-item v-b-modal.modalAccountForm>成為讀者</d-nav-item>
-              <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template slot="button-content">Me </template>
+              <d-nav-item v-b-modal.modalAccountForm v-if="!navbarState.accessToken">成為讀者</d-nav-item>
+              <b-nav-item-dropdown right v-if="navbarState.accessToken">
+                <template slot="button-content">{{ navbarState.user.name.last }} {{ navbarState.user.name.first }}</template>
                 <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                <b-dropdown-item v-on:click="userSignout">Sign Out</b-dropdown-item>
               </b-nav-item-dropdown>
             </d-navbar-nav>
           </d-collapse>
@@ -25,9 +24,30 @@
     </div>
 
     <div>
-      <b-modal id="modalAccountForm" v-bind:title="accountFormText">
-        <signinForm v-if="accountFormType === 'signin'" />
-        <registerForm v-if="accountFormType === 'register'" />
+      <b-modal id="modalAccountForm" v-bind:title="navbarState.accountFormText" hide-footer hide-header-close>
+        <d-button-group class="btn-group special">
+            <d-button
+              outline
+              v-on:click="switchAccountFormType('register')"
+              v-bind:class="{ 'active':  navbarState.accountFormType  === 'register' }"
+            >
+              成為讀者
+            </d-button>
+            <d-button 
+              outline
+              v-on:click="switchAccountFormType('signin')"
+              v-bind:class="{ 'active':  navbarState.accountFormType  === 'signin' }"
+            >
+              登入 TheChanger
+            </d-button>
+        </d-button-group>
+
+        <p></p>
+
+        <div>
+          <signinForm v-if="navbarState.accountFormType === 'signin'" />
+          <registerForm v-if="navbarState.accountFormType === 'register'" />
+        </div>
       </b-modal>
     </div>
 
@@ -35,6 +55,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import signinForm from '@/components/_navbar/signin'
 import registerForm from '@/components/_navbar/register'
 
@@ -42,33 +64,32 @@ export default {
   name: 'navbar',
   data () {
     return {
-      accountFormType: '',
-      showAccountForm: '',
-      accountFormText: ''
     }
   },
-  methods: {
-    init () {
-      this.accountFormType = 'register'
-      this.showAccountForm = false
-      this.accountFormText = '成為讀者'
-    },
-    changeAccountFormStatus (status) {
-      this.showAccountForm = status
-    },
-    switchAccountForm (type) {
-      this.accountFormType = type
-
-      if (this.type === 'signin') {
-        this.accountFormText = '登入 TheChanger 改變者'
-      }
-      if (this.type === 'register') {
-        this.accountFormText = '成為讀者'
+  computed: {
+    ...mapState({
+      navbarState: 'navbar'
+    }),
+    hideAccountFormModal () {
+      if (navbarState.accessToken) {
+        this.$ref['modalAccountForm'].hide()
       }
     }
   },
   mounted () {
-    this.init()
+    this.checkLoginStatus()
+  },
+  methods: {
+    switchAccountFormType (type) {
+      this.$store.commit('navbar/setAccountFormType', type)
+      this.$store.commit('navbar/setAccountFormText', type)
+    },
+    userSignout () {
+      this.$store.dispatch('navbar/userSignout')
+    },
+    checkLoginStatus () {
+      this.$store.dispatch('navbar/checkLoginStatus')
+    }
   },
   components: {
     signinForm,
@@ -79,4 +100,11 @@ export default {
 
 
 <style>
+.btn-group.special {
+  display: flex;
+}
+
+.special .btn {
+  flex: 1
+}
 </style>
