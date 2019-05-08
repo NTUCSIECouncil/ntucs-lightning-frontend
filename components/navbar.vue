fb9e8045aa4109521a21a6298193931e3146e5f8
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-09 02:20:46 +0800

    - add two icons

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,7 +3,13 @@
     <div>
       <d-navbar toggleable="lg" type="light" class="shadow-sm">
         <div class="container">
-          <d-navbar-brand>TheChanger 改變者</d-navbar-brand>
+          <d-navbar-brand>
+            <img
+              src="/theChangerLogov2.png"
+              alt="TheChanger Logo"
+              width="130px;"
+            >
+          </d-navbar-brand>
 
           <d-navbar-toggle target="nav-collapse"></d-navbar-toggle>
 
@@ -11,11 +17,16 @@
             <!-- Right aligned nav items -->
             <d-navbar-nav class="ml-auto">
               <d-nav-item href="#">成為作者</d-nav-item>
-              <d-nav-item v-b-modal.modalAccountForm v-if="!navbarState.accessToken">成為讀者</d-nav-item>
-              <b-nav-item-dropdown right v-if="navbarState.accessToken">
+              <d-nav-item v-b-modal.modalAccountForm v-if="!navbarState.isLoggedIn">成為讀者</d-nav-item>
+              <d-nav-item v-if="navbarState.isLoggedIn && navbarState.user.role === 'admin'">管理介面</d-nav-item>
+              <b-nav-item-dropdown right v-if="navbarState.isLoggedIn">
                 <template slot="button-content">{{ navbarState.user.name.last }} {{ navbarState.user.name.first }}</template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item v-on:click="userSignout">Sign Out</b-dropdown-item>
+                <b-dropdown-item v-if="!navbarState.user.isVerified">驗證我的帳號</b-dropdown-item>
+                <b-dropdown-divider />
+                <b-dropdown-item href="#">我的組織</b-dropdown-item>
+                <b-dropdown-item href="#">帳號設定</b-dropdown-item>
+                <b-dropdown-divider />
+                <b-dropdown-item v-on:click="userSignout">登出</b-dropdown-item>
               </b-nav-item-dropdown>
             </d-navbar-nav>
           </d-collapse>
@@ -24,7 +35,13 @@
     </div>
 
     <div>
-      <b-modal id="modalAccountForm" v-bind:title="navbarState.accountFormText" hide-footer hide-header-close>
+      <b-modal 
+        id="modalAccountForm"
+        v-bind:title="navbarState.accountFormText"
+        hide-footer 
+        hide-header-close
+        v-if="!navbarState.isLoggedIn"
+      >
         <d-button-group class="btn-group special">
             <d-button
               outline
@@ -69,12 +86,7 @@ export default {
   computed: {
     ...mapState({
       navbarState: 'navbar'
-    }),
-    hideAccountFormModal () {
-      if (navbarState.accessToken) {
-        this.$ref['modalAccountForm'].hide()
-      }
-    }
+    })
   },
   mounted () {
     this.checkLoginStatus()
