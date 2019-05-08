3755097cc129f7f9c3d91f9c2ac51a236336b25a
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-09 00:19:37 +0800

    - add vuex support

---
+++ store/navbar/index.js
@@ -0,0 +1,86 @@
+
+// initial state
+const state = {
+  accountFormType: 'register',
+  accountFormText: '成為讀者',
+
+  accessToken: '',
+  user: {
+    name: {
+      preferred: '',
+      last: '',
+      first: ''
+    }
+  }
+}
+
+// getters
+const getters = {
+}
+
+// actions
+const actions = {
+  checkLoginStatus ({ commit }) {
+    let accessToken = this.$storage.getLocalStorage('accessToken') || ''
+    let user = this.$storage.getLocalStorage('user') || ''
+    commit('setUserAccessToken', accessToken)
+    commit('setUserData', user)
+  },
+  userSignout ({ commit }) {
+    commit('removeUserAccessToken')
+  },
+  userSignin ({ commit }, payload) {
+    this.$axios.post('/auth/signin', { data: payload })
+      .then(i => {
+        let userData = i.data.data
+        commit('setUserAccessToken', userData.accessToken)
+        delete userData.accessToken
+        commit('setUserData', userData)
+      })
+  },
+  userRegister ({ commit }, payload) {
+    this.$axios.post('/auth/register', { data: payload })
+      .then(i => {
+        let userData = i.data.data
+        commit('setUserAccessToken', userData.accessToken)
+        delete userData.accessToken
+        commit('setUserData', userData)
+      })
+  }
+}
+
+// mutations
+const mutations = {
+  setAccountFormType (state, payload) {
+    state.accountFormType = payload
+  },
+  setAccountFormText (state, payload) {
+    if (payload === 'signin') {
+      state.accountFormText = '登入 TheChanger 改變者'
+    }
+    if (payload === 'register') {
+      state.accountFormText = '成為讀者'
+    }
+  },
+  setUserAccessToken (state, payload) {
+    this.$storage.setLocalStorage('accessToken', payload)
+    state.accessToken = payload
+  },
+  setUserData (state, payload) {
+    this.$storage.setLocalStorage('user', payload)
+    state.user = payload
+  },
+  removeUserAccessToken (state) {
+    this.$storage.removeLocalStorage('accessToken')
+    this.$storage.removeLocalStorage('user')
+    state.accessToken = ''
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
