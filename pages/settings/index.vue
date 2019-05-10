9b3a4e8d99160e543340ca651e3f9334d7f9d13b
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-11 00:26:31 +0800

    - add settings page

---
+++ pages/settings/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col md="3">
+          <settingsInfoColumn
+            v-bind:userData="userData"
+          />
+        </b-col>
+        <b-col md="4" offset-md="1">
+          
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import settingsInfoColumn from "@/components/settings/infoColumn";
+
+export default {
+  middleware: 'authRequired',
+  name: 'settings',
+  data () {
+    return {
+      userData: {}
+    }
+  },
+  methods: {
+    getUserData () {
+      this.$axios.get('/users/me', {
+        headers: {
+          authorization: this.$storage.getLocalStorage('accessToken')
+        }
+      }).then(i => {
+        let userData = i.data.data
+        this.userData = userData
+      })
+    }
+  },
+  mounted() {
+    this.getUserData()
+  },
+  components: {
+    settingsInfoColumn
+  }
+}
+</script>
+
