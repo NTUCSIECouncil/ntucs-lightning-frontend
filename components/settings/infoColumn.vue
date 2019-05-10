087f034f4c7015cf85820927bf4545052ba815f7
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-11 00:29:09 +0800

    - add settings info column component

---
+++ components/settings/infoColumn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <d-card>
+      <d-card-body>
+        <div class="profilePhoto">
+          <d-img fluid thumbnail src="https://placeimg.com/200/200/any" />
+        </div>
+        <p></p>
+        <div class="name">
+          <b>{{userData.lastName}} {{userData.firstName}}</b>
+        </div>
+      </d-card-body>
+    </d-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'settingsInfoColumn',
+  props: ['userData']
+}
+</script>
