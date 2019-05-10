69dfc8b5203cc4fa92f1128fa5987fadfa077152
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-11 00:27:12 +0800

    - add mobile navbar component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,19 @@
   <div>
     <navbar />
     <nuxt />
+    <smNavbar />
   </div>
 </template>
 
 <script>
 import navbar from '@/components/navbar'
+import smNavbar from '@/components/smNavbar'
 
 export default {
   name: 'default',
   components: {
-    navbar
+    navbar,
+    smNavbar
   }
 }
 </script>
