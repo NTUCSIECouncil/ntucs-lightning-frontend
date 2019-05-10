7e0c4d0bfc3283906a370e1b1f91afb35b8ef189
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-11 00:28:56 +0800

    - fix d-button-group with SSR problem

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,73 +1,76 @@
 <template>
   <div>
-    <div>
-      <d-navbar toggleable="lg" type="light" class="shadow-sm">
-        <div class="container">
-          <d-navbar-brand>
-            <img
-              src="/theChangerLogov2.png"
-              alt="TheChanger Logo"
-              width="130px;"
+    <no-ssr>
+      <div class="navbar-thechanger">
+        <d-navbar toggleable="lg" type="light" class="shadow-sm">
+          <div class="container">
+            <d-navbar-brand
+              href="/"
             >
-          </d-navbar-brand>
+              <img
+                src="/theChangerLogov2.png"
+                alt="TheChanger Logo"
+                width="130px;"
+              >
+            </d-navbar-brand>
 
-          <d-navbar-toggle target="nav-collapse"></d-navbar-toggle>
+            <d-navbar-toggle target="nav-collapse"></d-navbar-toggle>
 
-          <d-collapse id="nav-collapse" is-nav>
-            <!-- Right aligned nav items -->
-            <d-navbar-nav class="ml-auto">
-              <d-nav-item href="#">成為作者</d-nav-item>
-              <d-nav-item v-b-modal.modalAccountForm v-if="!navbarState.isLoggedIn">成為讀者</d-nav-item>
-              <d-nav-item v-if="navbarState.isLoggedIn && navbarState.user.role === 'admin'">管理介面</d-nav-item>
-              <b-nav-item-dropdown right v-if="navbarState.isLoggedIn">
-                <template slot="button-content">{{ navbarState.user.name.last }} {{ navbarState.user.name.first }}</template>
-                <b-dropdown-item v-if="!navbarState.user.isVerified">驗證我的帳號</b-dropdown-item>
-                <b-dropdown-divider />
-                <b-dropdown-item href="#">我的組織</b-dropdown-item>
-                <b-dropdown-item href="#">帳號設定</b-dropdown-item>
-                <b-dropdown-divider />
-                <b-dropdown-item v-on:click="userSignout">登出</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </d-navbar-nav>
-          </d-collapse>
-        </div>
-      </d-navbar>
-    </div>
+            <d-collapse id="nav-collapse" is-nav>
+              <!-- Right aligned nav items -->
+              <d-navbar-nav class="ml-auto">
+                <d-nav-item href="#">成為作者</d-nav-item>
+                <d-nav-item v-b-modal.modalAccountForm v-if="!navbarState.isLoggedIn">成為讀者</d-nav-item>
+                <d-nav-item v-if="navbarState.isLoggedIn && navbarState.user.role === 'admin'">管理介面</d-nav-item>
+                <b-nav-item-dropdown right v-if="navbarState.isLoggedIn">
+                  <template slot="button-content">{{ navbarState.user.name.last }} {{ navbarState.user.name.first }}</template>
+                  <b-dropdown-item v-if="!navbarState.user.isVerified">驗證我的帳號</b-dropdown-item>
+                  <b-dropdown-divider />
+                  <b-dropdown-item href="#">我的組織</b-dropdown-item>
+                  <b-dropdown-item href="/settings/">帳號設定</b-dropdown-item>
+                  <b-dropdown-divider />
+                  <b-dropdown-item v-on:click="userSignout">登出</b-dropdown-item>
+                </b-nav-item-dropdown>
+              </d-navbar-nav>
+            </d-collapse>
+          </div>
+        </d-navbar>
+      </div>
 
-    <div>
-      <b-modal 
-        id="modalAccountForm"
-        v-bind:title="navbarState.accountFormText"
-        hide-footer 
-        hide-header-close
-        v-if="!navbarState.isLoggedIn"
-      >
-        <d-button-group class="btn-group special">
-            <d-button
-              outline
-              v-on:click="switchAccountFormType('register')"
-              v-bind:class="{ 'active':  navbarState.accountFormType  === 'register' }"
-            >
-              成為讀者
-            </d-button>
-            <d-button 
-              outline
-              v-on:click="switchAccountFormType('signin')"
-              v-bind:class="{ 'active':  navbarState.accountFormType  === 'signin' }"
-            >
-              登入 TheChanger
-            </d-button>
-        </d-button-group>
+      <div>
+        <b-modal 
+          id="modalAccountForm"
+          v-bind:title="navbarState.accountFormText"
+          hide-footer 
+          hide-header-close
+          v-if="!navbarState.isLoggedIn"
+        >
+          <b-button-group class="btn-group special">
+              <d-button
+                outline
+                v-on:click="switchAccountFormType('register')"
+                v-bind:class="{ 'active':  navbarState.accountFormType  === 'register' }"
+              >
+                成為讀者
+              </d-button>
+              <d-button 
+                outline
+                v-on:click="switchAccountFormType('signin')"
+                v-bind:class="{ 'active':  navbarState.accountFormType  === 'signin' }"
+              >
+                登入 TheChanger
+              </d-button>
+          </b-button-group>
 
-        <p></p>
+          <p></p>
 
-        <div>
-          <signinForm v-if="navbarState.accountFormType === 'signin'" />
-          <registerForm v-if="navbarState.accountFormType === 'register'" />
-        </div>
-      </b-modal>
-    </div>
-
+          <div>
+            <signinForm v-if="navbarState.accountFormType === 'signin'" />
+            <registerForm v-if="navbarState.accountFormType === 'register'" />
+          </div>
+        </b-modal>
+      </div>
+    </no-ssr>
   </div>
 </template>
 
@@ -112,6 +115,9 @@ export default {
 
 
 <style>
+.navbar-thechanger {
+  margin-bottom: 20px;
+}
 .btn-group.special {
   display: flex;
 }
