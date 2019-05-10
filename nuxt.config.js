77e5903563da79e89a0ff4307acb33397829e143
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-11 04:45:19 +0800

    - add font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ module.exports = {
     ],
     link: [
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css' },
-      { rel: 'stylesheet', href: 'https://unpkg.com/shards-ui@latest/dist/css/shards.min.css' }
+      { rel: 'stylesheet', href: 'https://unpkg.com/shards-ui@latest/dist/css/shards.min.css' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css' }
     ]
   },
 
