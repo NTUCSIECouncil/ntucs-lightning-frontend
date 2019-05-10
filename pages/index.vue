4b6a8916051aef25d50b34c955a1f2e8761c2c03
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-11 00:27:23 +0800

    - remove HUG TITLE

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
   <section class="container">
-    <h1 class="text-center" style="padding-top: 50px;">
-       TheChanger 改變者
-    </h1>
-
     <p>
       Dear friends, We are still building our platform.
     </p>
