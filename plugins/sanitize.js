4d252bf172a06ed3e97a6c10c2387199cd614f7f
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-14 03:42:20 +0800

    - modify to add new filter policy when showing html in detl. artl.

--- plugins/sanitize.js
+++ plugins/sanitize.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueSanitize from 'vue-sanitize'
 
 const defaultOptions = {
-  allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+  allowedTags: ['h3', 'h4', 'h5', 'blockquote', 'p', 'a', 'ul', 'ol',
     'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div'
   ],
   allowedAttributes: {
