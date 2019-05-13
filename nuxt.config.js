078600926f8bcc5b84c4c834fc78db88347e1c2c
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-14 03:43:06 +0800

    - add vue2-medium-editor and their css file

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ module.exports = {
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css' },
       { rel: 'stylesheet', href: 'https://unpkg.com/shards-ui@latest/dist/css/shards.min.css' },
       { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif+TC' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif+TC' },
+      { rel: 'stylesheet', href: '//cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css' },
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/medium-editor/5.22.1/css/themes/tim.css' }
     ]
   },
 
@@ -41,7 +43,8 @@ module.exports = {
     { src: '~/plugins/shardsVue', ssr: true },
     { src: '~/plugins/bootstrap', ssr: true },
     { src: '~/plugins/vueNotifications', ssr: true },
-    { src: '~/plugins/sanitize', ssr: false }
+    { src: '~/plugins/sanitize', ssr: false },
+    { src: '~/plugins/mediumEditor', ssr: false }
   ],
 
   /*
