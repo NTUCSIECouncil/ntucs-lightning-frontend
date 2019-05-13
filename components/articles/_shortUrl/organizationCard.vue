31e765e5409c11fe016938c6d80882b50e6b88ed
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-14 03:41:51 +0800

    - add org card in detl. article page

--- components/articles/_shortUrl/organizationCard.vue
+++ components/articles/_shortUrl/organizationCard.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    <div id="organizationCard">
-      <div 
-        id="coverPhotoWrap"
-        v-bind:style="{ 'background-image': `url(${article.organization.coverPhoto})` }"
-      >
-      </div>
-      <div id="infoWrap">
-        {{article.organization.name}}
-      </div>
-    </div>
+    <b-containter id="organizationCard">
+      <b-row>
+        <b-col
+          id="infoWrap"
+          lg="2"
+          md="3"
+          sm="3"
+          xs="2"
+          cols="4"
+        >
+          <b-img
+            id="profilePhoto"
+            v-bind:src="article.organization.profilePhoto" 
+            rounded="circle"
+            width="100%"
+            height="100%"
+          >
+          </b-img>
+        </b-col>
+        <b-col
+          id="infoWrap"
+          lg="10"
+          md="9"
+          sm="9"
+          xs="10"
+          cols="8"
+        >
+          <h4 id="organizationName">{{article.organization.name}}</h4>
+          <p>
+            <i class="fas fa-map-marker-alt"></i>
+            {{article.organization.school.name}}
+          </p>
+          <blockquote>
+            {{article.organization.intro}}
+          </blockquote>
+        </b-col>
+      </b-row>
+    </b-containter>
   </div>
 </template>
 
@@ -22,11 +50,17 @@ export default {
 
 <style scoped>
 #organizationCard {
+  margin: 15px;
 }
-#coverPhotoWrap {
-  min-height: 10vh;
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
+#profilePhoto {
+  margin-left: 15px;
+}
+#infoWrap > h4, blockquote, p {
+  font-family: 'Noto Serif TC', serif;
+  line-height: 1.3em;
+  letter-spacing: 2px;
+}
+#organizationName {
+  margin-bottom: 3px;
 }
 </style>
