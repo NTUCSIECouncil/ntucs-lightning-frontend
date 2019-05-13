25bcaeced1b6aeb65a728bc9875ffeb563a22d67
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-14 03:43:17 +0800

    - add new page add artl.

---
+++ pages/articles/new.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <no-ssr>
+      <b-container>
+        <b-row>
+          <b-col lg="9">
+            <articleNewEditor />
+          </b-col>
+          <b-col lg="3">
+            <articleNewConfig />
+          </b-col>
+        </b-row>
+      </b-container>
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+import articleNewEditor from '@/components/articles/new/articleNewEditor'
+import articleNewConfig from '@/components/articles/new/articleNewConfig'
+
+export default {
+  middleware: [
+    'authRequired',
+    'authRequiredRoleOrgUser'
+  ],
+  name: 'articleNewPage',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+  },
+  mounted () {
+    
+  },
+  components: {
+    articleNewEditor,
+    articleNewConfig
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
