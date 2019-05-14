f9fddd508a344e8372b6a5a4ef1069967b1ab06b
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-05-15 02:09:01 +0800

    - change ui of cards

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -8,23 +8,28 @@
           sm="6"
           xs="6"
           cols="12"
+          id="cardWrap"
+          class="d-flex align-items-stretch"
           v-for="article in articleList" 
           v-bind:key="article.id"
         >
           <d-card v-on:click="accessFullArticle(article)">
             <d-card-img :src="article.coverPhoto" top />
             <d-card-body>
-               <p>
-                 ＃{{article.tag.name || '未分類文章'}}
-               </p>
+              <span id="tag">＃{{article.tag.name || '未分類文章'}}</span>
                <h5 id="card-title">
                  {{article.title}}
                </h5>
+               <small>
+                 {{article.intro}}
+               </small>
                <p></p>
-               <d-btn v-on:click="accessFullArticle(article)">
-                 詳細閱讀
-               </d-btn>
             </d-card-body>
+              <d-card-footer>
+                <d-btn v-on:click="accessFullArticle(article)">
+                  詳細閱讀
+                </d-btn>
+              </d-card-footer>
           </d-card>
         </b-col>
       </b-row>
@@ -64,6 +69,28 @@ export default {
     font-size: 1.2em;
     line-height: 1.3em;
     letter-spacing: 2px;
+  }
+  .card-body {
+    margin-top: -20px;
+  }
+  #tag {
+    color: blue;
+    line-height: 2em;
+  }
+  #cardWrap {
+    padding-top: 2em;
+  }
+  #cardWrap > .card {
+    min-height: 350px;
+    position: relative;
+  }
+  #accessToFullArticleButton {
+    position: absolute;
+    margin-bottom: 30px;
+    bottom: 0;
+  }
+  .card-footer {
+    background-color: white;
   }
 </style>
 
