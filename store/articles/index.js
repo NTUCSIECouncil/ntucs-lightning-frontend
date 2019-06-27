03b1253343514a186465899e288f89f293029e65
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-06-27 19:43:16 +0800

    - update store

--- store/articles/index.js
+++ store/articles/index.js
@@ -16,23 +16,22 @@ const actions = {
   async nuxtServerInit ({ dispatch }) {
   },
   newArticle ({ commit }) {
-    this.$axios.post('/articles/new', {}, {
+    return this.$axios.post('/articles/new', {}, {
       headers: {
         authorization: this.$storage.getLocalStorage('accessToken')
       }
     }).then(i => {
       commit('setNewArticleId', i.data.data.articleId)
+      return i
     }).catch(e => {
       commit('setAddArticleFailed')
+      throw e
     })
   }
 }
 
 // mutations
 const mutations = {
-  setNewArticleId (state, payload) {
-    state.newArticleId = payload
-  },
   setAddArticleFailed (state) {
     state.addArticleFailed = true
   }
