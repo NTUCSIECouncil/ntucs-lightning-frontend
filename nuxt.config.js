f390bf63058d6a56efe9f6f457cc6f0ea21622de
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-06-30 10:04:54 +0800

    - fix og problem

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,9 +18,9 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no' },
       { hid: 'description', name: 'description', content: 'TheChanger is a content platform that go with outstanding content which is write by student.' },
 
-      { property: 'og:title', content: 'TheChanger 改變者' },
-      { property: 'og:url', content: 'https://thechanger.me/' },
-      { property: 'og:description', content: 'TheChanger is a content platform that go with outstanding content which is write by student.' }
+      { hid: 'og:title', property: 'og:title', content: 'TheChanger 改變者' },
+      { hid: 'og:url', property: 'og:url', content: 'https://thechanger.me/' },
+      { hid: 'og:description', property: 'og:description', content: 'TheChanger is a content platform that go with outstanding content which is write by student.' }
 
       // PWA CONFIG BELOW THIS LINE
     ],
