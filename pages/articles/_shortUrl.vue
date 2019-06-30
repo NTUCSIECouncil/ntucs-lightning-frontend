da5008ee3e70c8526409252b65f3b7dbc401f28a
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-06-30 09:18:29 +0800

    - add og

--- pages/articles/_shortUrl.vue
+++ pages/articles/_shortUrl.vue
@@ -59,8 +59,11 @@ export default {
     return {
       title: this.articleRaw.title,
       meta: [
-        { hid: 'description', name: 'description', content: this.articleRaw.title },
-        { hid: 'og:description', name: 'og:description', content: this.articleRaw.title }
+        { hid: 'description', name: 'description', content: this.articleRaw.intro },
+
+        { hid: 'og:title', name: 'og:title', content: this.articleRaw.title },
+        { hid: 'og:description', name: 'og:description', content: this.articleRaw.intro },
+        { hid: 'og:image', name: 'og:image', content: this.articleRaw.coverPhoto }
       ]
     }
   },
