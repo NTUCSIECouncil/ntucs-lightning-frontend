e33d4d89f29ac24e302fd942d1ce2d43905014bd
Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
2019-06-30 19:07:52 +0800

    - add error "page"
    Yes, that's not a layout. although that is in layouts folder.

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="errorWrap d-block mx-auto">
+      <h1 v-if="error.statusCode === 404">
+        Page Not Found.
+      </h1>
+      <h1 v-if="error.statusCode === 403">
+        Restricted Area.
+      </h1>
+      <h1 v-else>
+        Something went wrong.
+      </h1>
+      <p></p>
+      <nuxt-link to="/">Back to Main Page</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+<style scoped>
+.errorWrap {
+  margin-top: 1rem;
+  background-color: rgba(240,240,240);
+  padding: 1.5rem;
+  text-align: center;
+}
+</style>
