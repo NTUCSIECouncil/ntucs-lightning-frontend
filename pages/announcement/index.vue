fb9cffe416734b11b519d831e484facce5d33fb7
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-09-07 15:57:20 +0800

    add some tailwindcss

--- pages/announcement/index.vue
+++ pages/announcement/index.vue
@@ -1,52 +1,53 @@
 <template>
   <div class="announcement-wrapper">
-    <h1>公告</h1>
-    <h1>Announcement</h1>
-    <table>
+    <h1 class="leading-10">公告</h1>
+    <h1 class="leading-loose">Announcement</h1>
+    <table class="m-12">
       <thead>
         <tr>
-          <td class="post-date">日期</td>
-          <td class="post-title">標題</td>
-          <td class="post-group">發布部門</td>
+          <td class="post-date table-text text-white">日期</td>
+          <td class="post-title table-text text-white">標題</td>
+          <td class="post-group table-text text-white">發布部門</td>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="article in articleList.slice((nowPage-1)*10,nowPage*10)" :key="article.articleId" @click.prevent="showArticle(article)">
-          <td class="post-date">{{parseCreateTime(article.createdAt)}}</td>
-          <td class="post-title">{{article.title}}</td>
-          <td class="post-group">{{article.organization.name}}</td>
+        <tr class="hover:bg-light hover:bg-opacity-50" v-for="article in articleList.slice((nowPage-1)*10,nowPage*10)" 
+              :key="article.articleId" @click.prevent="showArticle(article)">
+          <td class="post-date table-text">{{parseCreateTime(article.createdAt)}}</td>
+          <td class="post-title table-text">{{article.title}}</td>
+          <td class="post-group table-text">{{article.organization.name}}</td>
         </tr>
       </tbody>
     </table>
-    <div class="page-select-wrapper">
+    <div class="page-select-wrapper text-center">
       <div v-if="lessPageMode">
-        <div class="page" @click="lastPage()">&#60;</div>
-        <div class="page" :class="{'page-underline':nowPage==page}" 
+        <div class="page page-choose" @click="lastPage()">&#60;</div>
+        <div class="page page-choose" :class="{'page-underline':nowPage==page}" 
                   v-for="page in pageCount" :key="page" @click="goToPage(page)" >
           {{page}}
         </div>
-        <div class="page" @click="nextPage()">&#62;</div>
+        <div class="page page-choose" @click="nextPage()">&#62;</div>
       </div>
 
       <div v-else>
-        <div class="page" @click="lastPage()">&#60;</div>
-        <div class="page" :class="{'page-underline':nowPage==1}" @click="goToPage(1)" >1</div>
+        <div class="page page-choose" @click="lastPage()">&#60;</div>
+        <div class="page page-choose" :class="{'page-underline':nowPage==1}" @click="goToPage(1)" >1</div>
         
-        <div class='split-page' v-if="nowPage-1 > 2">...</div>
+        <div class='split-page page-choose' v-if="nowPage-1 > 2">...</div>
 
-        <div class="page" v-if="nowPage-1 > 1" @click="goToPage(nowPage-1)">{{nowPage-1}}</div>
-        <div class="page page-underline" v-if="nowPage > 1 && nowPage < pageCount" @click="goToPage(nowPage)">{{nowPage}}</div>
-        <div class="page" v-if="nowPage+1 < pageCount" @click="goToPage(nowPage+1)">{{nowPage+1}}</div>
-        <div class='split-page' v-if="nowPage+2 < pageCount">...</div>
+        <div class="page page-choose" v-if="nowPage-1 > 1" @click="goToPage(nowPage-1)">{{nowPage-1}}</div>
+        <div class="page page-underline page-choose" v-if="nowPage > 1 && nowPage < pageCount" @click="goToPage(nowPage)">{{nowPage}}</div>
+        <div class="page page-choose" v-if="nowPage+1 < pageCount" @click="goToPage(nowPage+1)">{{nowPage+1}}</div>
+        <div class='split-page page-choose' v-if="nowPage+2 < pageCount">...</div>
         
-        <div class="page" :class="{'page-underline':nowPage==pageCount}" @click="goToPage(pageCount)">{{pageCount}}</div>
-        <div class="page" @click="nextPage()">&#62;</div>
+        <div class="page page-choose" :class="{'page-underline':nowPage==pageCount}" @click="goToPage(pageCount)">{{pageCount}}</div>
+        <div class="page page-choose" @click="nextPage()">&#62;</div>
       </div>
     </div>
-    <div class="pop-wrapper" v-if="articleOpen" @click.prevent="articleOpen=false">
+    <div class="pop-wrapper top-0 left-0 w-full h-full" v-if="articleOpen" @click.prevent="articleOpen=false">
       <div class="pop-card">
-        <h1>{{readingArticle?readingArticle.title:''}}</h1>
-        <article class="pop-content">
+        <h1 class="leading-loose">{{readingArticle?readingArticle.title:''}}</h1>
+        <article class="pop-content text-black font-medium">
           {{readingArticle?readingArticle.intro:''}}
         </article>
       </div>
@@ -59,9 +60,13 @@ export default {
   name: 'announcementPage',
   data() {
     return {
-      articleList:[],
+      articleList:[
+        {articleId:'1', createdAt:'2021-02-12', title:'title', organization:{name:'學術部'}, intro:'hihihihihihihi'},
+        {articleId:'1', createdAt:'2021-02-12', title:'title', organization:{name:'學術部'}},
+        {articleId:'1', createdAt:'2021-02-12', title:'title', organization:{name:'學術部'}},
+      ],
       nowPage: 1,
-      pageCount: 0,
+      pageCount: 10,
       articleOpen: false,
       readingArticle: undefined
     }
@@ -101,63 +106,35 @@ export default {
       return this.pageCount <= 5;
     }
   },
-  mounted(){
+  /*mounted(){
     this.getArticles();
-  }
+  }*/
 }
 </script>
 <style scoped>
 * {
   font-family: 'Source Han Serif TC';
 }
-h1 {
-  font-family: Source Han Serif TC;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 48px;
-  line-height: 69px;
-  text-align: center;
+h1{
+  @apply text-5xl font-bold text-center;
 }
 table {
-  margin: 0 auto;
+  margin-left: auto;
+  margin-right: auto;
 }
 thead {
   background-color: #023047;
 }
-thead td{
-  font-family: Source Han Serif TC;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 34px;
-  flex: none;
-  order: 0;
-  flex-grow: 0;
+tbody td {
+  color: #0A1629;
+  border-bottom: 1px solid #000000;
+}
+.table-text {
+  @apply text-xl font-bold leading-4 h-12;
   margin: 0px 10px;
-  color: #ffffff;
 }
 tr {
   transition: .3s;
-}
-tbody td {
-  position: static;
-  font-family: Source Han Serif TC;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 34px;
-  color: #0A1629;
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  margin: 0px 10px;
-  border-bottom: 1px solid #000000;
-}
-tbody tr:hover {
-  background-color: rgba(142, 202, 230, .5);
-}
-td {
-  height: 52px;
 }
 .post-date {
   width: 220px;
@@ -170,41 +147,24 @@ td {
 }
 .page-select-wrapper {
   margin: 0 auto;
-  text-align: center;
 }
 .page-select-wrapper .page{
-  display: inline-block;
   width: 45px;
   height: 45px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   padding: 4px;
-  font-family: Source Han Serif TC;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 34px;
   color: #0A1629;
-  text-align: center;
   transition: .3s;
 }
 .page-select-wrapper .split-page{
-  display: inline-block;
   width: 45px;
   height: 45px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   padding: 4px;
-  font-family: Source Han Serif TC;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 34px;
   color: #0A1629;
-  text-align: center;
 }
+.page-choose {
+  @apply inline-block font-bold text-center text-xl items-center justify-center;
+}
+
 .page-underline {
   text-decoration: underline #023047;
 }
@@ -216,10 +176,6 @@ td {
 }
 .pop-wrapper {
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
   z-index: 2;
   background: rgba(196, 196, 196, .28);
   backdrop-filter: blur(10px);
@@ -233,8 +189,6 @@ td {
   transform: translateX(-50%) translateY(-50%);
   background: #ffffff;
   z-index: 3;
-  flex-direction: column;
-  align-items: flex-start;
   padding: 52px 110px;
   box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
   animation: popup .5s;
@@ -263,11 +217,7 @@ td {
 .pop-content{
   width: 860px;
   height: 88px;
-  font-family: Source Han Serif TC;
-  font-style: normal;
-  font-weight: 500;
   font-size: 16px;
   line-height: 23px;
-  color: #000000;
 }
 </style>
